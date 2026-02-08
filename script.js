0fe74850b93f8250f50e0a2c7ce4fddbf7b8338a
sreyajkmr2003-a11y <sreyajkmr2003@gmail.com>
2026-02-08 12:40:54 +0530

    Refactor letter interaction and update cat image

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 const title = document.getElementById("letter-title");
-const catVideo = document.getElementById("letter-cat");
+const catImg = document.getElementById("letter-cat");
 const buttons = document.getElementById("letter-buttons");
 const finalText = document.getElementById("final-text");
 const letterWindow = document.querySelector(".letter-window");
@@ -21,7 +21,7 @@ envelope.addEventListener("click", () => {
 
 // Move NO button
 const moveNoBtn = () => {
-  const distance = 250;
+  const distance = 100;
   const angle = Math.random() * Math.PI * 2;
 
   const x = Math.cos(angle) * distance;
@@ -36,13 +36,7 @@ noBtn.addEventListener("touchstart", moveNoBtn);
 // YES clicked
 yesBtn.addEventListener("click", () => {
   title.textContent = "I knew you’d say YES 😁😘💕";
-
-  // Change cat video
-  catVideo.innerHTML = `
-    <source src="cat-happy.mp4" type="video/mp4">
-  `;
-  catVideo.load();
-  catVideo.play();
+  catImg.src = "cosytales-cute.gif";
 
   letterWindow.classList.add("final");
   buttons.style.display = "none";
